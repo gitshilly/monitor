de1fb4d9efa2e931e2c39a62444cd9caf6807bfa
gitshilly <735328375@qq.com>
2019-01-09 15:55:29 +0800

    监控视图

---
+++ src/js/EMCommon.js
@@ -0,0 +1,10 @@
+/**
+* @Author: zhangye
+* @Date:   2017-07-06 16:20:42
+* @Email:   zhangye1@emoney.com
+* @Version:   1.0
+* @Last Modified by:   zhangye
+* @Last Modified time: 2017-07-06 16:20:42
+*/
+
+'use strict';
